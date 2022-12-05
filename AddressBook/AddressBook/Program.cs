7477fb369abe2912289cac2b308b9872a4ee7e3e
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-05 23:09:32 +0530

    Add Person Details

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using AddressBook.Model;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook");
+            Person person = new Person();
+
+            person.FirstName = "Rajesh";
+            person.LastName = "Kumar";
+            person.MobileNumber = 2555665566;
+            person.Email = "rajesh55@gmail.com";
+            person.Address = "kurawali mainpuri ";
+            person.City = "mainpuri";
+            person.zip = 205265;
+            person.State = "Uttarpradesh";
+        }
+    }
+}
---
+++ AddressBook/AddressBook/Repository/ContactDetails.cs
@@ -0,0 +1,33 @@
+﻿using AddressBook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Repository
+{
+    internal class ContactDetails
+    {
+        public Dictionary<long, Person> contactDetailDictionary = new Dictionary<long, Person>();
+        public void AddPersonDetails(Person contactDetails)
+        {
+            contactDetailDictionary.Add(Convert.ToInt64(contactDetails.MobileNumber), contactDetails);
+        }
+        public void DisplayContact()
+        {
+            foreach (var item in contactDetailDictionary)
+            {
+                Console.WriteLine("First Name is : " + item.Value.FirstName);
+                Console.WriteLine("Last Name is : " + item.Value.LastName);
+                Console.WriteLine("Mobile Number is : " + item.Value.MobileNumber);
+                Console.WriteLine("Email ID is : " + item.Value.Email);
+                Console.WriteLine("Address is : " + item.Value.Address);
+                Console.WriteLine("City is : " + item.Value.City);
+                Console.WriteLine("State is : " + item.Value.State);
+                Console.WriteLine("Zip Code is : " + item.Value.zip);
+                Console.WriteLine();
+            }
+        }
+    }
+}
